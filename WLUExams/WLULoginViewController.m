3ecfaccba7d9d68c55ef6781c6ff7badf2037424
Erin Cramer <erincramer@Erins-Mac-mini.local>
2014-04-13 16:26:47 -0400

    updated code for ios 6 compatibility

--- WLUExams/WLULoginViewController.m
+++ WLUExams/WLULoginViewController.m
@@ -182,7 +182,7 @@
 
 - (void) animateTextField: (UITextField*) textField up: (BOOL) up
 {
-    const int movementDistance = 90; // tweak as needed
+    const int movementDistance = 110; // tweak as needed
     const float movementDuration = 0.3f; // tweak as needed
     
     int movement = (up ? -movementDistance : movementDistance);
